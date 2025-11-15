228f9bd4fec3d07711f18f04d22169f0e005a6ed
NickolasPratama <marioaldywijaya_2327240082@mhs.mdp.ac.id>
2025-11-15 17:15:28 +0700

    i

---
+++ src/routes/router.ts
@@ -0,0 +1,28 @@
+import { createBrowserRouter } from "react-router";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        children : [
+            {
+                index: true,
+                lazy : {
+                    Component : async () => {
+                        const component = await import("../pages/movies/Movie.tsx")
+                        return component.default
+                    }
+                }
+            },{
+                path: "add-movie",
+                lazy : {
+                    Component : async () => {
+                        const component = await import("../pages/movies/AddMovie.tsx")
+                        return component.default
+                    }
+                }
+            }
+        ]
+    }
+])
+
+export default router
