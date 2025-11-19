e95e99e0ca988a429c3651ec1a5be5fa7c3f5027
NickolasPratama <marioaldywijaya_2327240082@mhs.mdp.ac.id>
2025-11-19 20:36:38 +0700

    t

--- src/pages/movies/Movie.tsx
+++ src/pages/movies/Movie.tsx
@@ -1,10 +1,81 @@
+import { useState } from "react";
 import { NavLink } from "react-router";
+import ApiClient from "../../utils/ApiClient";
+import { useCallback, useEffect } from "react";
+import Table from 'react-bootstrap/Table';
+
+interface Movie{
+  _id: string,
+  judul: string,
+  tahunRilis: string,
+  sutradara: string,
+  createdAt: string,
+  updatedAt: string
+}
 
 function Movie() {
-  return <div>
-    <h2>Movie Page</h2>
-    <NavLink to="/add-movie" className="btn btn-primary">Add Movie</NavLink>
-  </div>;
+  const [movies, setMovies] = useState<Movie[]>([])
+  const [loading, setLoading] = useState<boolean>(true)
+
+  const fetchMovies = useCallback(async () => {
+    const response = await ApiClient.get("/movie")
+
+    if(response.status == 200){
+      setMovies(response.data.data)
+      setLoading(false)
+    }
+  },[])
+
+  useEffect(() => {
+    fetchMovies()
+  },[fetchMovies])
+
+  const handleDelete = async (movieId: string) => {
+    const response = await ApiClient.delete(`/movie/${movieId}`);
+
+    if (response.status == 200) {
+      fetchMovies()
+    }
+
+  }
+  return <div className="container mx-auto">
+    <div className="d-flex justify-content-between mb-3">
+      <h2>Movie Page</h2>
+        <NavLink to="/add-movie" className="btn btn-primary">Add Movie</NavLink>
+    </div>
+    <div>
+    <Table striped>
+      <thead>
+        <th>No</th>
+        <th>Judul</th>
+        <th>Tahun Rilis</th>
+        <th>Sutradara</th>
+        <th>Aksi</th>
+        </thead>
+        <tbody>
+          {
+          loading && <tr><td colSpan={5}>Loading...</td>
+          </tr>
+          }
+          
+          {
+          movies.length > 0 && movies.map((movie, index) => {
+            return <tr key={movie._id}>
+              <td>{index + 1}</td>
+              <td>{movie.judul}</td>
+              <td>{movie.tahunRilis}</td>
+              <td>{movie.sutradara}</td>
+              <td>
+                <button className="btn btn-danger" onClick={()=> handleDelete(movie._id)}>Delete</button>
+              </td>
+              </tr>
+
+          })}
+        </tbody>
+    </Table>
+  </div>
+  </div>
+  
 }
 
 export default Movie;
---
+++ src/utils/ApiClient.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const ApiClient = axios.create({
+  baseURL: "http://localhost:3000/api",
+  headers : {"accept" : "application/json"}
+    })
+
+export default ApiClient;
